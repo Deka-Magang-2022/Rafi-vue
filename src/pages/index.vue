caa19539fc60ef09fc653fea1ecf98458eaecc61
rafinurrizki <kaptraf369@gmail.com>
2022-07-18 12:04:12 +0800

    portofolio

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -31,6 +31,7 @@
 				<div class="text-capitalize text-center	 ">
 					<p>No Telepon : 0821-5520-4061</p>
 					<p>Email : kaptraf369@gmail.com</p>
+					<p><a href="https://github.com/Deka-Magang-2022/Rafi-vue/pulls">* GitHub *</a></p>
 				</div>
 			<div>
 				<h1 class="uppercase font-hold text-xl mt-10">Hobby</h1>
@@ -42,9 +43,9 @@
 			<div>
 				<h1 class="uppercase font-hold text-xl mt-10">Education</h1>
 				<div class="text-capitalize text-center ">
-					<p>SD : SDN 005</p>
-					<P>SMP : MTS SULAIMAN YASIN</P>
-					<P>SMK : TI AIRLANGGA SAMARINDA</P>
+					<p><a href="https://www.google.com/maps/place/Gedung+Pertemuan+SDN+005+Samarinda+Ilir/@-0.4934727,117.1621823,20.67z/data=!4m12!1m6!3m5!1s0x2df67fa51beb797f:0x545bae808dedfa6e!2sSMP+NEGERI+6+SAMARINDA!8m2!3d-0.4911339!4d117.1612508!3m4!1s0x2df67f4e0eadf647:0x8f9a35f10941ce04!8m2!3d-0.4934272!4d117.1622486"> * SDN 005</a> </p>
+					<P><a href="https://www.google.com/maps/place/Madrasah+Tsanawiyah+Sulaiman+Yasin/@-0.4909931,117.1571426,17z/data=!3m1!4b1!4m5!3m4!1s0x2df67f6f3984dca3:0xd7a44486b8fde19b!8m2!3d-0.4909985!4d117.1593313"> * MTS SULAIMAN YASIN</a></P>
+					<P><a href="https://www.google.com/maps/place/SMKTI+Airlangga/@-0.4851169,117.1462529,17z/data=!3m1!4b1!4m5!3m4!1s0x2df67f1489bfeb63:0xe1ef6869246e2125!8m2!3d-0.4851223!4d117.1484416">* SMK TI AIRLANGGA</a></P>
 				</div>
 			</div>
 			<div>
@@ -54,6 +55,7 @@
 				<p>- css</p>
 				<p>- html</p>
 				<p>- javascript</p>
+				<p>- css (tailwind)</p>
 				</div>
 			</div>
 			<div>
@@ -62,6 +64,7 @@
 			</div>
 			<div class="">
 				<a href="todo-list">Next</a>
+				<a href="color-switcher">Color Swicther</a>
 
 			</div>
 		</div>
